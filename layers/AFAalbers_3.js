c7724fb6687a23d26a75c7b554004762e0d05d14
adityanism <140131082+adityanism@users.noreply.github.com>
2023-12-03 11:18:37 +0700

    Add files via upload

---
+++ layers/AFAalbers_3.js
@@ -0,0 +1,1 @@
+var json_AFAalbers_3 = {"type":"FeatureCollection","name":"AFAalbers_3","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"Ket":"Lembaga pendidikan","Tahun":"1930","Alamat":"Daendelsweg / Jl. Batavia"},"geometry":{"type":"Point","coordinates":[107.638777257969195,-6.913956827012258]}}]}
